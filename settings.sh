dc2bf567ca11a48114939581daaa42ba04d205ff
kenny <kenny8zeng@gmail.com>
2016-11-17 16:32:20 +0800

    remove vundle

--- settings.sh
+++ settings.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+git clone https://github.com/gmarik/vundle.git vimfiles/bundle/vundle
+
 [ -L ~/.vim-using ]  && rm -f ~/.vim-using
 [ -L ~/.vim ]        && rm -f ~/.vim
 [ -f ~/.viminfo ]    && rm -f ~/.viminfo
