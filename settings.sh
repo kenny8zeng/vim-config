a1d64a7f38224b87dff32042c980ff363f1ee458
kenny <kenny8zeng@gmail.com>
2016-11-17 16:23:31 +0800

    add settings sh file

---
+++ settings.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+[ -L ~/.vim-using ]  && rm -f ~/.vim-using
+[ -L ~/.vim ]        && rm -f ~/.vim
+[ -f ~/.viminfo ]    && rm -f ~/.viminfo
+[ -L ~/.viminfo ]    && rm -f ~/.viminfo
+[ -L ~/.vimrc ]      && rm -f ~/.vimrc
+
+ln -s ~/.vim-config ~/.vim-using
+
+ln -s ~/.vim-using/vimfiles  ~/.vim
+ln -s ~/.vim-using/_viminfo  ~/.viminfo
+ln -s ~/.vim-using/_vimrc    ~/.vimrc
+
